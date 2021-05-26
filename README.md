0c7722a0eee9da1953c2e4e6762ec10c90c376c5
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2021-05-25 20:15:37 -0600

    Adjustments Made it Work
    Signed-off-by: WalkingLibrary <jumboofawarrior@gmail.com>

---
+++ README.md
@@ -0,0 +1,33 @@
+# Journey Map Sticher
+![Sauce](https://img.shields.io/badge/100%25-Spaghetti%20Code-orange)
+![Build Status](https://img.shields.io/badge/Build-Passing-green)
+
+
+# Stitches your Journey Map Together
+
+Build
+```
+Linux
+./gradlew shadowjar
+
+
+Windows
+
+gradlew.bat shadowjar
+
+```
+After Building it Should be In /build/libs/{Jar File Name}
+
+
+Usage
+```
+
+java -jar <jar name> <x> <z> <max width>
+
+```
+
+When Run correctly the Program will create an output image of the Given Parameters.
+
+License
+----
+![AUR license](https://img.shields.io/badge/License-MIT-blue)
--- src/main/java/com/jumbodinosaurs/journeymapsticher/Main.java
+++ src/main/java/com/jumbodinosaurs/journeymapsticher/Main.java
@@ -1,40 +1,138 @@
 package com.jumbodinosaurs.journeymapsticher;
 
-import java.io.File;
+import com.jumbodinosaurs.devlib.log.LogManager;
+import com.jumbodinosaurs.devlib.util.GeneralUtil;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        if(args.length < 4)
+        int journeyMapRegionImageSize = 512;
+        if(args.length < 3)
         {
-            System.out.println("Usage: java -jar <jar name> <journey map image dir> <x> <z> <max width>");
+            System.out.println("Usage: java -jar <jar name> <x> <z> <max width>");
             return;
         }
-        
+    
         int x, z, maxWidth;
-        File journeyMapDir;
         try
         {
-            journeyMapDir = new File(args[0]);
-            x = Integer.parseInt(args[1]);
-            z = Integer.parseInt(args[2]);
-            maxWidth = Integer.parseInt([3]);
+            x = Integer.parseInt(args[0]);
+            z = Integer.parseInt(args[1]);
+        
+        
+            maxWidth = Integer.parseInt(args[2]);
+        
+            if(maxWidth <= 16 * 32)
+            {
+                System.out.println("width Not Big Enough");
+                return;
+            }
         }
         catch(Exception e)
         {
-            
+        
             System.out.println("Arguments Given were not Valid");
             return;
         }
-        
-        Point
     
-        for(File journeyMapImage: journeyMapDir.listFiles())
+        int xOffset, zOffset;
+    
+        xOffset = x + (maxWidth / 2);
+        zOffset = z + (maxWidth / 2);
+    
+        BufferedImage largerImage = new BufferedImage(maxWidth, maxWidth, BufferedImage.TYPE_3BYTE_BGR);
+    
+        System.out.println("Making Image at X: " + x + " Z: " + z + " and " + maxWidth + " Blocks Wide");
+        File journeyMapDir = GeneralUtil.checkFor(GeneralUtil.userDir, "Input", true);
+        for(File journeyMapImage : journeyMapDir.listFiles())
         {
+            try
+            {
+            
+                int currentFilesRegionX, currentFilesRegionZ;
+                String fileName = journeyMapImage.getName();
+                fileName = fileName.split("\\.")[0];
+                fileName = fileName.split(",")[0] + " " + fileName.split(",")[1];
+                Scanner scanner = new Scanner(fileName);
+                currentFilesRegionX = scanner.nextInt();
+                currentFilesRegionZ = scanner.nextInt();
+            
+            
+                //Add Image to Bigger Image
+                byte[] imageBytes = readPhoto(journeyMapImage);
+                BufferedImage currentImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
+            
+            
+                for(int rows = 0; rows < journeyMapRegionImageSize; rows++)
+                {
+                    for(int columns = 0; columns < journeyMapRegionImageSize; columns++)
+                    {
+                        int colorAT = currentImage.getRGB(rows, columns);
+                    
+                        int offsetX, offsetZ;
+                    
+                        offsetX = ((currentFilesRegionX * 32 * 16) + rows) + xOffset;
+                        offsetZ = ((currentFilesRegionZ * 32 * 16) + columns) + zOffset;
+                    
+                        if(offsetX >= 0 && offsetX <= largerImage.getWidth() - 1)
+                        {
+                            if(offsetZ >= 0 && offsetZ <= largerImage.getHeight() - 1)
+                            {
+                                largerImage.setRGB(offsetX, offsetZ, colorAT);
+                            }
+                        }
+                    
+                    }
+                }
+            
+            
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                continue;
+            }
+        
         
         }
+        File outputFile = GeneralUtil.checkFor(GeneralUtil.userDir, "output.png");
+    
+        try
+        {
+            System.out.println("Image Saved at " + outputFile.getAbsolutePath());
+            ImageIO.write(largerImage, "png", outputFile);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
         
-        
+        }
+    
+    
     }
+    
+    public static byte[] readPhoto(File file)
+    {
+        try
+        {
+            byte[] imageBytes = new byte[(int) file.length()];
+            InputStream imageStream = new BufferedInputStream(new FileInputStream(file));
+            imageStream.read(imageBytes, 0, imageBytes.length);
+            imageStream.close();
+            return imageBytes;
+        }
+        catch(Exception e)
+        {
+            LogManager.consoleLogger.error("Error Reading Photo", e);
+            
+        }
+        return null;
+    }
+    
 }
