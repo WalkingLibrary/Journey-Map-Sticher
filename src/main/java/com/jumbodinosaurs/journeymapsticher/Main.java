6215cf32d458ce422de844b80a77d48b2f561318
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2021-06-06 09:39:19 -0600

    Adjustments - fixed issue with offset
    Signed-off-by: WalkingLibrary <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/journeymapsticher/Main.java
+++ src/main/java/com/jumbodinosaurs/journeymapsticher/Main.java
@@ -43,13 +43,15 @@ public class Main
     
         int xOffset, zOffset;
     
-        xOffset = x + (maxWidth / 2);
-        zOffset = z + (maxWidth / 2);
+    
+        xOffset = x - (maxWidth / 2);
+        zOffset = z - (maxWidth / 2);
     
     
         int regionRange = (maxWidth / 16 / 32) + 2;
     
         int regionX, regionZ;
+    
     
         regionX = x / 16 / 32;
         regionZ = z / 16 / 32;
@@ -62,41 +64,50 @@ public class Main
         {
             try
             {
-            
+    
+    
                 int currentFilesRegionX, currentFilesRegionZ;
+    
+                //PArsing of the File name
                 String fileName = journeyMapImage.getName();
                 fileName = fileName.split("\\.")[0];
                 fileName = fileName.split(",")[0] + " " + fileName.split(",")[1];
                 Scanner scanner = new Scanner(fileName);
+                //Gets the current files region
                 currentFilesRegionX = scanner.nextInt();
                 currentFilesRegionZ = scanner.nextInt();
-            
-            
-                //Add Image to Bigger Image
-                byte[] imageBytes = readPhoto(journeyMapImage);
-                BufferedImage currentImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
-            
+    
+    
+                //Culling before reading in the image
+    
+                //if the difference between the specified area x and the current file is greater than the range
                 if(!(Math.abs(regionX - currentFilesRegionX) < regionRange))
                 {
                     continue;
                 }
-            
+    
                 if(!(Math.abs(regionZ - currentFilesRegionZ) < regionRange))
                 {
                     continue;
                 }
-            
+    
+                //Add Image to Bigger Image
+                byte[] imageBytes = readPhoto(journeyMapImage);
+    
+    
+                BufferedImage currentImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
+    
                 for(int rows = 0; rows < journeyMapRegionImageSize; rows++)
                 {
                     for(int columns = 0; columns < journeyMapRegionImageSize; columns++)
                     {
                         int colorAT = currentImage.getRGB(rows, columns);
-                    
+            
                         int offsetX, offsetZ;
-                    
-                        offsetX = ((currentFilesRegionX * 32 * 16) + rows) + xOffset;
-                        offsetZ = ((currentFilesRegionZ * 32 * 16) + columns) + zOffset;
-                    
+            
+                        offsetX = ((currentFilesRegionX * 32 * 16) + rows) - xOffset;
+                        offsetZ = ((currentFilesRegionZ * 32 * 16) + columns) - zOffset;
+            
                         if(offsetX >= 0 && offsetX <= largerImage.getWidth() - 1)
                         {
                             if(offsetZ >= 0 && offsetZ <= largerImage.getHeight() - 1)
@@ -104,7 +115,7 @@ public class Main
                                 largerImage.setRGB(offsetX, offsetZ, colorAT);
                             }
                         }
-                    
+            
                     }
                 }
             
